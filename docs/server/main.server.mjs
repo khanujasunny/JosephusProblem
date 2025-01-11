3c98f91476be9dbbe8960052eea7a7196124d5a0
Sunny <sunkha7@gmail.com>
2025-01-12 02:26:35 +0530

    Code refactored to support girhub pages

--- docs/server/main.server.mjs
+++ docs/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-7RXWCUO2.mjs";import"./chunk-SGAGYLYR.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
+import{a}from"./chunk-PDQJJAQB.mjs";import"./chunk-Y2UIKFEZ.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
